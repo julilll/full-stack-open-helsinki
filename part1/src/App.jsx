ceb9f6d75273dcfedf99d7f79b6685edd011b4d9
julilll <jleschinskaya@gmail.com>
2024-03-01 23:53:54 +0200

    add more exercises to part 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,28 +2,30 @@ const Part = (props) => <p>{props.name} {props.hours}</p>
 
 const Header = (props) => <h1>{props.course}</h1>;
 
-const Content = (props) => <div>{props.exercises.map((exercise, index) => <Part key={index} name={exercise.name} hours={exercise.hours}/>)}</div>
+const Content = (props) => <div>{props.exercises.map((exercise, index) => <Part key={index} name={exercise.name} hours={exercise.hours} />)}</div>
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = (props) => <p>Number of exercises {props.parts.map(el => el.hours).reduce((total, value) => total += value)}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const exercises = [{
-    name: 'Fundamentals of React',
-    hours: 10
-  }, {
-    name: 'Using props to pass data',
-    hours: 7
-  }, {
-    name: 'State of a component',
-    hours: 14
-  }]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      hours: 10
+    }, {
+      name: 'Using props to pass data',
+      hours: 7
+    }, {
+      name: 'State of a component',
+      hours: 14
+    }]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content exercises={exercises}/>
-      <Total total={exercises.map(el => el.hours).reduce((total, value) => total += value)}/>
+      <Header course={course.name} />
+      <Content exercises={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
