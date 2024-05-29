730bebfa9f81a1b3c3a314c1478e8691286f3121
julilll <julilevi1205@gmail.com>
2024-05-29 22:36:27 +0300

    add task 6.3 - 6.8

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,13 @@
+import AnecdoteForm from "./AnecdoteForm";
+import AnecdoteList from "./AnecdoteList";
+
+const App = () => {
+  return (
+    <>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </>
+  );
+};
+
+export default App;
